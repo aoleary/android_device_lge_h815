e863b82a8fc17b9860ec2e113ab8401582f03d12
aoleary <seanm187@gmail.com>
2018-08-18 23:05:35 +0200

    Atomize and simplify the H815 device tree

---
+++ aos.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from h815 device
+$(call inherit-product, device/lge/h815/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/aos/config/common.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := h815
+PRODUCT_NAME := aos_h815
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H815
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4_global_com" \
+    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
+
+BUILD_FINGERPRINT := "lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys"
+
