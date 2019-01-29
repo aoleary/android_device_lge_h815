cf1801ce85d60e585ec2787e7f89f410f85d57dc
aoleary <seanm187@gmail.com>
2019-01-29 05:08:01 +0000

    Simplify the lineage trees

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,19 @@
-$(call inherit-product, device/lge/h815/full_h815.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from h815 device
+$(call inherit-product, device/lge/h815/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := h815
 PRODUCT_NAME := lineage_h815
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-H815
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
